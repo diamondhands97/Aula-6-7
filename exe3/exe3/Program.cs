8db7f29f3f9b4439a66e774594a0aa900318bd68
Ruthless <tsb27development@gmail.com>
2022-03-18 15:26:33 +0000

    exe3

---
+++ exe3/exe3/Program.cs
@@ -0,0 +1,18 @@
+﻿
+const char Caract = 'X';
+const char Espaco = ' ';
+int larg = 5, comp = 10;
+for (int c = 1; c <= comp; c++) 
+    Console.Write(Caract);
+Console.WriteLine();
+for (int l = 1; l <= larg; l++)
+{
+    Console.Write(Caract);
+    for (int w = 2; w <= comp - 1; w++) 
+        Console.Write(Espaco);
+    Console.WriteLine(Caract);
+}
+for (int c = 1; c <= comp; c++)   
+    Console.Write(Caract);
+    Console.WriteLine();
+    
