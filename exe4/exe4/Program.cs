085d1cf1627c425770adcefb5eaf43ce0aaef124
Ruthless <tsb27development@gmail.com>
2022-03-18 15:44:26 +0000

    exe4

---
+++ exe4/exe4/Program.cs
@@ -0,0 +1,16 @@
+﻿const string Caract = "XXX";
+const string Espaco = "   ";
+int larg = 5, comp = 5;
+for (int c = 1; c <= comp; c++)
+    Console.Write(Caract);
+Console.WriteLine();
+for (int l = 1; l <= larg; l++)
+{
+    Console.Write(Caract);
+    for (int w = 1; w <= comp-2; w++)
+        Console.Write(Espaco);
+    Console.WriteLine(Caract);
+}
+for (int c = 1; c <= comp; c++)
+    Console.Write(Caract);
+Console.WriteLine();
