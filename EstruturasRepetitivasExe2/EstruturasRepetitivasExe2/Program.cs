787d5b99f0aebee14600242f02fe9628292a877c
Ruthless <tsb27development@gmail.com>
2022-03-18 15:09:39 +0000

    exe2

--- EstruturasRepetitivasExe2/EstruturasRepetitivasExe2/Program.cs
+++ EstruturasRepetitivasExe2/EstruturasRepetitivasExe2/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+int a,b = 0;
+
+do
+{
+	Console.WriteLine("Digite o valor Inicial do intervalo:");
+	a = Convert.ToInt32(Console.ReadLine());
+
+	Console.WriteLine("Digite o valor Final do intervalo:");
+	b = Convert.ToInt32(Console.ReadLine());
+
+}while (a > b);
+
+for (int i = a; i <= b; i++) 
+{
+	if (i % 2 != 0)
+	{
+		Console.WriteLine("{0}",i);
+	}
+}
