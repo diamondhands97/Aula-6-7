b1e1a6d3e2a18d0ea8f6134d8687fd53a041a11e
Ruthless <tsb27development@gmail.com>
2022-03-18 14:47:44 +0000

    Aula-6-7

---
+++ EstruturasRepetitivasExe1/EstruturasRepetitivasExe1/Program.cs
@@ -0,0 +1,11 @@
+﻿
+    Console.Write("Limite superior do intervalo de inteiros?");
+    int N = Convert.ToInt32(Console.ReadLine());
+    int Soma = 0;
+
+    for (int i = 1; i <= N; i++) 
+    {
+        Soma += i;
+    }
+
+    Console.WriteLine("Total da adição dos {0} inteiros={1}", N, Soma);
