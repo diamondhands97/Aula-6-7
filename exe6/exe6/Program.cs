42b3ba1eeb12f0ef353bf68e75b862d833b4602f
Ruthless <tsb27development@gmail.com>
2022-03-18 16:58:20 +0000

    exe6

--- exe6/exe6/Program.cs
+++ exe6/exe6/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = 0;
+int b = 1;
+int c = 0;
+
+for (int i = 2; i < 20; i++)
+{
+    c = a + b;
+    a = b;
+    b = c;
+        
+    
+    Console.WriteLine(a);
+}
+
